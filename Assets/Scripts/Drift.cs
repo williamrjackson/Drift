32b68f07ef8db3cf79c42e3d356817fe0089667c
Bill Jackson <billjackson@cakewalk.com>
2018-04-07 03:35:32 -0400

    Cleanup, commenting, handle collisions better
    When you hit a wall, causing you to stop moving, don't try to drift. you
    need to reset movement direction and start rebuilding speed.
    
    Renamed rotateSpeed variable
    
    Lots of comments

--- Assets/Scripts/Drift.cs
+++ Assets/Scripts/Drift.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Drift : MonoBehaviour {
     public float maxSpeed = 5f;
-    public float rotateSpeed = 200f;
+    public float accelerationTime = 1f;
+    public float rotateDegreesPerSec = 180f;
     public TouchAxisCtrl touchAxis;
     public TrailRenderer[] trails;
 
@@ -15,6 +16,8 @@ public class Drift : MonoBehaviour {
     void Start()
     {
         m_RigidBody = GetComponent<Rigidbody2D>();
+        // Velocity direction is parented by the car transform, which allows it to adopt it's rotations, 
+        // while simultaneously getting it's own rotation. That's the "drifty" part.
         m_VelDir = new GameObject().transform;
         m_VelDir.parent = transform;
         m_VelDir.localPosition = Vector3.zero;
@@ -22,24 +25,39 @@ public class Drift : MonoBehaviour {
     }
     void Update()
     {
+        // Set applied speed to the car's actual speed. This is for the cases where the car's travel
+        // is interrupted by a collision, and needs to build back up to max again.
         m_AppliedSpeed = m_RigidBody.velocity.magnitude;
+
+        // If we're not currently travelling at max speed...
         if (m_AppliedSpeed < maxSpeed)
         {
-            m_AppliedSpeed += maxSpeed * Time.deltaTime;
+            // See if we crashed into something, causing us to stop. Make sure we're not still trying to drift/skid. 
+            if (m_AppliedSpeed < .5f)
+                m_VelDir.localEulerAngles = Vector3.zero;
+            // Gradually build speed.
+            m_AppliedSpeed += maxSpeed * Time.deltaTime * accelerationTime;
         }
         float zVal;
+        // Represent the difference between the car's pointing direction and travel direction as a float ranging -1 to 1. 
         float angleOffset = Remap((Mathf.DeltaAngle(transform.eulerAngles.z, m_VelDir.transform.eulerAngles.z)), -90, 90, -1, 1);
         if (touchAxis.GetAxis("InverseHorizontal") == 0)
         {
+            // If not actively turning, straighten out gradually.
             zVal = transform.eulerAngles.z +  m_AppliedSpeed * angleOffset / 3;
         }
         else
         {
-            zVal = transform.eulerAngles.z + rotateSpeed * Time.deltaTime * touchAxis.GetAxis("InverseHorizontal");
+            // Turn over n degrees per second, scaled by joystick input value.
+            zVal = transform.eulerAngles.z + rotateDegreesPerSec * Time.deltaTime * touchAxis.GetAxis("InverseHorizontal");
         }
+        // Apply new rotation
         transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, zVal);
+        // Determine travel velocity. The car may be moving perfectly "sideways," traveling 90 deg from car's forward.
         m_VelDir.localEulerAngles = new Vector3(0, 0, Mathf.LerpAngle(m_VelDir.localEulerAngles.z, touchAxis.GetAxis("Horizontal") * 90f, Time.deltaTime));
 
+        // Manage the transparency of trail renderers. Not perfect, since the whole trail fades when the car straightens out.
+        // Ideally, "skidmarks" would be permanent.
         foreach (TrailRenderer t in trails)
         {
             Color newAlpha = t.startColor;
@@ -51,9 +69,11 @@ public class Drift : MonoBehaviour {
 
     void LateUpdate()
     {
+        // Move the car
         m_RigidBody.velocity = m_VelDir.up * m_AppliedSpeed;
     }
 
+    // Linear remap
     float Remap(float val, float srcMin, float srcMax, float destMin, float destMax)
     {
         return Mathf.Lerp(destMin, destMax, Mathf.InverseLerp(srcMin, srcMax, val));
