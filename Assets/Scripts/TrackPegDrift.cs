5c52ae73f8a88ea752d618740ce87f42a99e3952
Bill Jackson <billjackson@cakewalk.com>
2018-04-09 03:19:49 -0400

    Working radial fill

--- Assets/Scripts/TrackPegDrift.cs
+++ Assets/Scripts/TrackPegDrift.cs
@@ -1,31 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TrackPegDrift : MonoBehaviour {
 
     public Canvas RadialImageCanvas;
 
+    private bool m_bIsTriggered;
+    private Transform m_Captured;
+    private float m_InAngle;
+    private Image m_Radial;
+
 	// Use this for initialization
 	void Start () {
-		
+        m_Radial = RadialImageCanvas.GetComponentInChildren<Image>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+		if (m_bIsTriggered)
+        {
+            Vector3 dirBetween = m_Captured.transform.position - transform.position;
+            float angle = EnsurePositiveAngle(Mathf.Atan2(dirBetween.y, dirBetween.x) * Mathf.Rad2Deg);
+            float deltaAngle = Mathf.DeltaAngle(m_InAngle, angle);
+            float val = Remap(EnsurePositiveAngle(deltaAngle), 0, 360, 1, 0);
+            m_Radial.fillAmount = val;
+        }
+    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
+        m_bIsTriggered = true;
+        m_Captured = other.transform;
         RadialImageCanvas.transform.position = transform.position;
-        Vector2 tada = (other.transform.position - transform.position).normalized;
-        float angle = Mathf.Atan2(tada.x, tada.y) * Mathf.Rad2Deg;
-        RadialImageCanvas.transform.eulerAngles = new Vector3(RadialImageCanvas.transform.eulerAngles.x, RadialImageCanvas.transform.eulerAngles.y, angle); 
-        print("in");
+        Vector3 vectorTo = other.transform.position - transform.position;
+        float angle = EnsurePositiveAngle(Mathf.Atan2(vectorTo.y, vectorTo.x) * Mathf.Rad2Deg);
+        m_InAngle = angle;
+        RadialImageCanvas.transform.localEulerAngles = new Vector3(0,0,angle); 
     }
     void OnTriggerExit2D(Collider2D other)
     {
-        print("Out!");
+        m_bIsTriggered = false;
+        m_Radial.fillAmount = 0;
+    }
+
+    float Remap(float val, float srcMin, float srcMax, float destMin, float destMax)
+    {
+        return Mathf.Lerp(destMin, destMax, Mathf.InverseLerp(srcMin, srcMax, val));
+    }
+    public static float EnsurePositiveAngle(float inputAngle)
+    {
+        if (inputAngle < 0) { inputAngle += 360; }
+        return inputAngle;
     }
 }
