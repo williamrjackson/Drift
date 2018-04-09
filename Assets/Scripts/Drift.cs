71bbb89601b6885f212af24df3c5afc1b0b373ab
Bill Jackson <billjackson@cakewalk.com>
2018-04-08 20:15:17 -0400

    Taking the linerenderer trails; cleanup

--- Assets/Scripts/Drift.cs
+++ Assets/Scripts/Drift.cs
@@ -7,13 +7,16 @@ public class Drift : MonoBehaviour {
     public float accelerationTime = 1f;
     public float rotateDegreesPerSec = 180f;
     public TouchAxisCtrl touchAxis;
-    public float particleDistance = .1f;
-    public ParticleSystem[] particles;
+    public float trackSegLength = .15f;
+    public int traceSegCount = 100;
+    public Transform[] wheels;
+    public Material trailMaterial;
 
     Rigidbody2D m_RigidBody;
     private Transform m_VelDir;
     float m_AppliedSpeed = 0;
-    Vector3 m_LastParticlePos;
+    private List<WheelTrack> m_WheelTracks;
+    private Vector3 m_LastPos;
 
     void Start()
     {
@@ -24,8 +27,15 @@ public class Drift : MonoBehaviour {
         m_VelDir.parent = transform;
         m_VelDir.localPosition = Vector3.zero;
         m_VelDir.localEulerAngles = Vector3.zero;
+        m_LastPos = transform.position;
+        m_WheelTracks = new List<WheelTrack>();
+        for (int i = 0; i < wheels.Length; i++)
+        {
+            WheelTrack wheel = new WheelTrack();
+            wheel.Init(wheels[i], trailMaterial, traceSegCount);
+            m_WheelTracks.Add(wheel);
+        }
 
-        m_LastParticlePos = transform.position;
     }
     void Update()
     {
@@ -60,17 +70,18 @@ public class Drift : MonoBehaviour {
         // Determine travel velocity. The car may be moving perfectly "sideways," traveling 90 deg from car's forward.
         m_VelDir.localEulerAngles = new Vector3(0, 0, Mathf.LerpAngle(m_VelDir.localEulerAngles.z, touchAxis.GetAxis("Horizontal") * 90f, Time.deltaTime));
 
-        // Manage the transparency of particles. 
-        if (Vector3.Distance(transform.position, m_LastParticlePos) > particleDistance)
+        // If we've traveled the track segment lengh, make a new segment.
+        if (Vector3.Distance(transform.position, m_LastPos) > trackSegLength)
         {
-            m_LastParticlePos = transform.position;
+            // Remember this for next time to see how far we've traveled.
+            m_LastPos = transform.position;
+
+            // Determine track segment transparency. 
             Color newAlpha = Color.black;
-            newAlpha.a = Mathf.Abs(angleOffset);
-            var emitParams = new ParticleSystem.EmitParams();
-            emitParams.startColor = newAlpha;
-            foreach (ParticleSystem p in particles)
+            newAlpha.a = Mathf.Min(Mathf.Abs(angleOffset), .5f);
+            foreach (WheelTrack wheel in m_WheelTracks)
             {
-                p.Emit(emitParams, 1);
+                wheel.AddSegment(newAlpha);
             }
         }
     }
@@ -85,5 +96,53 @@ public class Drift : MonoBehaviour {
     float Remap(float val, float srcMin, float srcMax, float destMin, float destMax)
     {
         return Mathf.Lerp(destMin, destMax, Mathf.InverseLerp(srcMin, srcMax, val));
+    }
+
+    private class WheelTrack
+    {
+        private List<Transform> lines;
+        private Vector3 lastPos;
+        private Transform transform;
+        private Transform lineHolder;
+        private int lineIndex = 0;
+        private Material mat;
+        private int segmentCount;
+        
+        public void Init(Transform tf, Material material, int segCount)
+        {
+            lines = new List<Transform>();
+            transform = tf;
+            mat = material;
+            segmentCount = segCount;
+            lineHolder = new GameObject(transform.name + "Tracks").transform;
+        }
+        public void AddSegment(Color color)
+        { 
+            // Until we have enough segments, make new ones.
+            if (lines.Count < segmentCount)
+            {
+                GameObject go = new GameObject();
+                go.transform.parent = lineHolder;
+
+                // Set up line renderer
+                LineRenderer newLine = go.AddComponent<LineRenderer>();
+                newLine.material = mat;
+                newLine.startWidth = newLine.endWidth = .05f;
+                newLine.positionCount = 2;
+
+                // Add to our list of lines
+                lines.Add(go.transform);
+            }
+
+            // Set up the line the way we want it.
+            LineRenderer line = lines[lineIndex].GetComponent<LineRenderer>();
+            line.SetPosition(0, transform.position);
+            line.SetPosition(1, lastPos);
+            line.startColor = line.endColor = color;
+
+            lastPos = transform.position;
+            // Loop back to the oldest segment next time (unless there's room)
+            lineIndex = (lineIndex + 1) % segmentCount;
+        }
     }
 }
