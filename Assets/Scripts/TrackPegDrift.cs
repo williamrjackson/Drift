534ecfab8e52cb744b59328c2c72d8611f7f8c21
Bill Jackson <billjackson@cakewalk.com>
2018-04-08 22:49:43 -0400

    Experimenting with tracking successful peg loops

---
+++ Assets/Scripts/TrackPegDrift.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackPegDrift : MonoBehaviour {
+
+    public Canvas RadialImageCanvas;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        RadialImageCanvas.transform.position = transform.position;
+        Vector2 tada = (other.transform.position - transform.position).normalized;
+        float angle = Mathf.Atan2(tada.x, tada.y) * Mathf.Rad2Deg;
+        RadialImageCanvas.transform.eulerAngles = new Vector3(RadialImageCanvas.transform.eulerAngles.x, RadialImageCanvas.transform.eulerAngles.y, angle); 
+        print("in");
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        print("Out!");
+    }
+}
